f534ecd2bd6c10b42a0018ef409badfe6d7dda30
Harshadasonawane2002 <99062345+Harshadasonawane2002@users.noreply.github.com>
2022-02-11 20:55:05 +0530

    Create Print.cs

---
+++ Print.cs
@@ -0,0 +1,24 @@
+
+Harshadasonawane2002/Harshadasonawane2002 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+static void Main(string[] args)
+{
+For(int i=0; i<=5; i++)
+{
+ Console.WriteLine("read one, each one,teach one");
+  Console.ReadLine();
+}
+}
+    }
+}
