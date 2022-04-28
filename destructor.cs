7c186bc8154592034701f8d3ec49552f4869b936
Harshadasonawane2002 <99062345+Harshadasonawane2002@users.noreply.github.com>
2022-04-28 14:58:13 +0530

    Create destructor.cs

---
+++ destructor.cs
@@ -0,0 +1,24 @@
+//Program to demonstrate destructor in c#.net.
+Using System;
+namespaceCSharp_Shell
+{
+  Public class Program 
+{
+Public Program()
+             {
+		Console.WriteLine(“Constructor called”);
+}
+~Program()
+{
+	Console.WriteLine(“Destructor called”);
+	}
+   Public static void Main(string [] args)
+                       }
+Program p=new Program();
+}
+  }
+}
+
+
+
+
